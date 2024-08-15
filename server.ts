aa900c2dcd49a39d5c8de0252eadc363b3860cb5
Md Shakil Hossain <108464347+devshakilh@users.noreply.github.com>
2024-08-15 22:22:36 +0600

    Update server.ts

--- server.ts
+++ server.ts
@@ -25,7 +25,7 @@ app.use(express.json());
 
 
 app.use(cors({
-    origin: 'http://localhost:3000', // Replace with your frontend URL if different
+    origin: 'https://medicine-ecommerce.onrender.com', // Replace with your frontend URL if different
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true, // If you're using cookies or other credentials
   }));
